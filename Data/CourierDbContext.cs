7df16cc3d4254acd60187892cfa883d264872c5a
Asif Zaman <eramasif16@gmail.com>
2023-06-13 11:20:17 +0600

    Add project files.

---
+++ Data/CourierDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Courier_Management_System.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Courier_Management_System.Data
+{
+    public class CourierDbContext : DbContext
+    {
+        public CourierDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Shipment> Shipments { get; set; }
+    }
+}
