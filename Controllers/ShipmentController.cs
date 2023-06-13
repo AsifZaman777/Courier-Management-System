6a64f9f342a2d7ccb8e264eae0b8150e2c6e8ba4
Asif Zaman <eramasif16@gmail.com>
2023-06-13 13:05:39 +0600

    fix-admin and connect - local db

---
+++ Controllers/ShipmentController.cs
@@ -0,0 +1,46 @@
+﻿using Courier_Management_System.Data;
+using Courier_Management_System.Models;
+using Courier_Management_System.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Courier_Management_System.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        private readonly CourierDbContext courierDbContext;
+
+        public ShipmentController(CourierDbContext courierDbContext)
+        {
+            this.courierDbContext = courierDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddShipmentViewModel addShipmentRequest)
+        {
+            var shipment = new Shipment()
+            {
+                Id = Guid.NewGuid(),
+                ShipperName = addShipmentRequest.ShipperName,
+                ShipperAddress = addShipmentRequest.ShipperAddress,
+                ShipperEmail = addShipmentRequest.ShipperEmail,
+
+                ReceiverName = addShipmentRequest.ReceiverName,
+                ReceiverAddress = addShipmentRequest.ReceiverAddress,
+                ReceiverEmail = addShipmentRequest.ReceiverEmail,
+
+                ShipmentStatus = addShipmentRequest.ShipmentStatus,
+
+            };
+
+            await courierDbContext.Shipments.AddAsync(shipment);
+            await courierDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+
+        }
+    }
+}
