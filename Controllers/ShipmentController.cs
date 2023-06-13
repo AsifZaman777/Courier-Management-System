28e571a4ff1d24db6666c07b5886138e0891775a
Asif Zaman <eramasif16@gmail.com>
2023-06-14 00:57:36 +0600

    feat -update and view

--- Controllers/ShipmentController.cs
+++ Controllers/ShipmentController.cs
@@ -40,7 +40,7 @@ namespace Courier_Management_System.Controllers
 
             await courierDbContext.Shipments.AddAsync(shipment);
             await courierDbContext.SaveChangesAsync();
-            return RedirectToAction("Add");
+            return RedirectToAction("ShipmentList");
 
         }
 
@@ -51,8 +51,53 @@ namespace Courier_Management_System.Controllers
             return View(shipments);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> View(Guid Id)
+        {
+            var shipmentInfoAvailable = await courierDbContext.Shipments.FirstOrDefaultAsync(x => x.Id == Id);
 
- 
+            if(shipmentInfoAvailable != null)
+            {
+                var viewModel = new UpdateShipmentViewModel()
+                {
+                    Id = shipmentInfoAvailable.Id,
+                    ShipperName = shipmentInfoAvailable.ShipperName,
+                    ShipperAddress = shipmentInfoAvailable.ShipperAddress,
+                    ShipperEmail = shipmentInfoAvailable.ShipperEmail,
+
+                    ReceiverName = shipmentInfoAvailable.ReceiverName,
+                    ReceiverAddress = shipmentInfoAvailable.ReceiverAddress,
+                    ReceiverEmail = shipmentInfoAvailable.ReceiverEmail,
+
+                    ShipmentStatus = shipmentInfoAvailable.ShipmentStatus,
+                };
+                return await Task.Run (()=> View("View", viewModel));
+            }
+
+            return RedirectToAction("ShipmentList");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateShipmentViewModel model)
+        {
+            var shipments = await courierDbContext.Shipments.FindAsync(model.Id);
+
+            if(shipments != null)
+            {
+                shipments.ShipperName = model.ShipperName;
+                shipments.ShipperAddress = model.ShipperAddress;
+                shipments.ShipperEmail = model.ShipperEmail;
+                shipments.ReceiverName = model.ReceiverName;
+                shipments.ReceiverAddress = model.ReceiverAddress;
+                shipments.ReceiverEmail = model.ReceiverEmail;
+                shipments.ShipmentStatus = model.ShipmentStatus;
+
+               await courierDbContext.SaveChangesAsync();
+
+                return RedirectToAction("ShipmentList");
+            }
+            return RedirectToAction("ShipmentList");
+        }
 
     }
 }
